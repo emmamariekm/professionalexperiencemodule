0a54e6dda47da2a6b4789cec4e75fae74a77d1bc
emmamariekm <146450139+emmamariekm@users.noreply.github.com>
2026-09-22 00:06:51 +0100

    Enhance data.R with comments
    taking hand-written notes during project and integrated in comments to track thought process.

--- scripts/data.R
+++ scripts/data.R
@@ -1,19 +1,32 @@
+# arrow package allows large dataset .parquet files to be read in
 install.packages("arrow")
-library(arrow)
+library(arrow
+
+# data stored in object       
 data <-read_parquet("PCV_Stacked.parquet")
-head(data)
-ncol(data)
-nrow(data)  
-names(data)
-str(data)
-unique(data$product_name)
-unique(data$sub_product_name)
-unique(data$manufacture_site)
-unique(data$variant)
-length(unique(data$batch_number))
-length(unique(data$parameter))
-summary(data$weighted_avg_value)
-summary(table(data$batch_number))
-table(data$product_name, data$variant)
-save(data, file = "data.RData")
+        
+head(data) #inspect first lines of data
+ncol(data) #10 variables
+nrow(data)  #1315782 observations
+names(data) #ie. batch numbers, sites, parameters, weighted average values ect.
+str(data) #large R dataset table 
+unique(data$product_name) # 3 products, Prevenar, 13 & 20
+unique(data$sub_product_name) # 4 sub-products, drug product, PFS, MDV, SDV
+unique(data$manufacture_site) #two sites - Grange Castle, Puurs
+unique(data$variant) #6 variants, S01, S03, S04, S09V, S18C, S19A
+length(unique(data$batch_number)) #13608 unique batch numbers <100 rows per batch
+length(unique(data$parameter)) #132 unique parameters, multiple paramenters per batch
+summary(data$weighted_avg_value) #misleading right-skewed distribution due to average of all parameter values measured at different scales
+#also get number of NAs = 49848 = 3.8% missing data
+table(data$product_name, data$variant) #creating a table isolating out two variables as an example
+save(data, file = "data.RData") #save object in project folder
+
+# day 2 agenda: 
+#pivot the data wider
+#investigate NAs, conduct binary classification based on threshold
+#product distributions for individual parameters
+#create histograms in r studio for these distributions
+#investigate outliers & any skew-ness in the histograms
+        
+        
 
